0acbf6ac04c4d16109c6c193bf87d0357823a28e
VladislavRatslavCB <VladislavRatslavCB@users.noreply.github.com>
2024-03-12 14:04:32 +0200

    fix: more then 100 headers can be present in the request
    Replacing an ngx.req.get_headers() with ngx.req.raw_header().

--- kong/plugins/oidc/utils.lua
+++ kong/plugins/oidc/utils.lua
@@ -194,19 +194,7 @@ function M.injectHeaders(header_names, header_claims, sources)
 end
 
 function M.has_bearer_access_token()
-  local headers = ngx.req.raw_header()
-  local header
-
-  -- Split the raw headers into lines and iterate through them
-  for line in headers:gmatch("[^\r\n]+") do
-    -- Check if the current line starts with "Authorization:"
-    if line:find("^[Aa]uthorization:") then
-      -- Extract the value of the Authorization header
-      header = line:match(": (.+)$")
-      break
-    end
-  end
-  
+  local header = ngx.req.raw_header():match("[\r\n][Aa]uthorization:%s*(.-)[\r\n]")
   if header and header:find(" ") then
     local divider = header:find(' ')
     if string.lower(header:sub(0, divider-1)) == string.lower("Bearer") then
